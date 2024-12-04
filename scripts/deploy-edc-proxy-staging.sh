48fb5036f4768b133e22972bd0679b5ff7ccd6c1
Karagkounis Dimitris <dkaragkounis@intracom-telecom.com>
2024-12-04 11:40:46 +0200

    add deployment configuration for the staging environment

---
+++ scripts/deploy-edc-proxy-staging.sh
@@ -0,0 +1,11 @@
+set -e
+
+export IMAGE_TAG="${1:-v0.0.1}"
+
+echo "Deploying K8S resources..."
+export KUBECONFIG="/cygdrive/c/Users/dkaragkounis/Desktop/Omega-X/omega-x.yaml"
+cat k8s/edc-proxy-staging.yaml| envsubst | kubectl -n marketplace apply -f -
+kubectl -n marketplace wait --for=condition="Available" --timeout=60s deployment/edc-proxy-deployment-staging
+echo "Deployment is now available."
+kubectl -n marketplace get pods
+echo "K8S resources deployment completed."
