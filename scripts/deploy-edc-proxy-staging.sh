373d53c75237b703e636b14fa3d8be8b376b0f8a
Karagkounis Dimitris <dkaragkounis@intracom-telecom.com>
2025-04-01 14:14:42 +0300

    remove KUBECONFIG from deployment scripts

--- scripts/deploy-edc-proxy-staging.sh
+++ scripts/deploy-edc-proxy-staging.sh
@@ -3,7 +3,6 @@ set -e
 export IMAGE_TAG="${1:-v0.0.1}"
 
 echo "Deploying K8S resources..."
-export KUBECONFIG="/cygdrive/c/Users/dkaragkounis/Desktop/Omega-X/omega-x.yaml"
 cat deployments/edc-proxy-staging.yaml| envsubst | kubectl -n marketplace apply -f -
 kubectl -n marketplace wait --for=condition="Available" --timeout=60s deployment/edc-proxy-deployment-staging
 echo "Deployment is now available."
--- scripts/deploy-migration-job.sh
+++ scripts/deploy-migration-job.sh
@@ -7,7 +7,6 @@ set -e
 export IMAGE_TAG="${1:-v0.0.1}"
 
 echo "Automigrate database tables schema..."
-export KUBECONFIG="/cygdrive/c/Users/dkaragkounis/Desktop/Omega-X/omega-x.yaml"
 # kubectl -n marketplace apply -f deployments/edc-proxy-migration-job.yaml
 cat deployments/edc-proxy-migration-job.yaml| envsubst | kubectl -n marketplace apply -f -
 kubectl -n marketplace wait --for=condition="Complete" --timeout=60s job/edc-proxy-migration-job
